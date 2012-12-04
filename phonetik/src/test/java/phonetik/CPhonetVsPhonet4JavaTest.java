742d7fe7de5af35f0f79a74bbc290cc7c5c42be4
blob79 <thomas.andreas.jung@googlemail.com>
2012-12-04 16:21:03 +0100

    added test with german dictionary and surnames test data (from the phonet4j project)

--- phonetik/src/test/java/phonetik/CPhonetVsPhonet4JavaTest.java
+++ phonetik/src/test/java/phonetik/CPhonetVsPhonet4JavaTest.java
@@ -1,16 +1,43 @@
 package phonetik;
 
-import junit.framework.TestCase;
-import net.java.quickcheck.generator.PrimitiveGeneratorSamples;
+import static net.java.quickcheck.generator.PrimitiveGenerators.letterStrings;
+import static net.java.quickcheck.generator.iterable.Iterables.toIterable;
+import static org.junit.Assert.assertEquals;
+
+import java.net.URL;
+
+import org.junit.Test;
+
 import phonet.CPhonet;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Resources;
 
-public class CPhonetVsPhonet4JavaTest extends TestCase {
 
-	public void test1() {
+public class CPhonetVsPhonet4JavaTest {
+
+	@Test public void generated() {
 		Phonet1 phonet1 = new Phonet1();
 		CPhonet cPhonet = new CPhonet();
-		String in = PrimitiveGeneratorSamples.anyLetterString();
-		assertEquals(phonet1.code(in), cPhonet.phonet(in));
+		for (String in : toIterable(letterStrings())) {
+			assertEquals(phonet1.code(in), cPhonet.phonet(in));
+		}
+		
 	}
+	
+	@Test public void phoneticTestData() throws Exception {
+		CPhonet cPhonet = new CPhonet();
+		Phonet1 phonet1 = new Phonet1();
+		for(String resource: new String[] { "/german.txt", "/surnames.txt" }) {
+			URL url = getClass().getResource(resource);
+			for (String nextLine : CharStreams.readLines(
+					Resources.newReaderSupplier(url, Charsets.UTF_8))){
+		        String c = cPhonet.phonet(nextLine);
+				String java = phonet1.code(nextLine);
+				assertEquals(c, java);
+		    }
+		}
+    }
+	
 }
